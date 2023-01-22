31a2b941ae0cda64f1aaccd664a21317feb17e1b
youjia <luowenjia.lwj@alibaba-inc.com>
2023-01-22 19:26:04 +0800

    feat: to#fiber

---
+++ packages/react-reconciler/src/beginWork.ts
@@ -0,0 +1,6 @@
+import { FiberNode } from './fiber';
+
+// 递 比较返回子fiberWork
+export const beginWork = (fiber: FiberNode): any => {
+	//
+};
---
+++ packages/react-reconciler/src/completeWork.ts
@@ -0,0 +1,6 @@
+import { FiberNode } from './fiber';
+
+// 归
+export const completeWork = (fiber: FiberNode): any => {
+	//
+};
